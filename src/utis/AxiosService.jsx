3fd7583b684d6b44c368f797b30609761c4be6e5
Gomathy <gomathy17012002@gmail.com>
2024-05-08 17:47:50 +0530

    commited

--- src/utis/AxiosService.jsx
+++ src/utis/AxiosService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let AxiosService = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://diet-suggestion-app-backend-zjxa.onrender.com",
 
   headers: {
     "Content-Type": "application/json",
